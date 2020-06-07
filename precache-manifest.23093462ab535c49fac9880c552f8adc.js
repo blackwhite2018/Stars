8a7c9c704ef8b1db3ceef3979f0ea66abfec2296
Sergei M <black_white2018@rambler.ru>
2020-06-07 21:05:56 +0300

    Updates

---
+++ precache-manifest.23093462ab535c49fac9880c552f8adc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a71ddb09fa6f3e0aa256253c21bb9cc",
+    "url": "/Stars/index.html"
+  },
+  {
+    "revision": "000c7d91d7d586622ebe",
+    "url": "/Stars/static/css/main.8b82bcf8.chunk.css"
+  },
+  {
+    "revision": "8dba1bb2da3d858c7d4e",
+    "url": "/Stars/static/js/2.27c1d965.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Stars/static/js/2.27c1d965.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "000c7d91d7d586622ebe",
+    "url": "/Stars/static/js/main.15a5bef8.chunk.js"
+  },
+  {
+    "revision": "34a74f716bc169bbebbd",
+    "url": "/Stars/static/js/runtime-main.48930cbe.js"
+  }
+]);
